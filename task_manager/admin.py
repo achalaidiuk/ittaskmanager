6b4df0747a28e5c2d575efdb57e90c7076f5b714
achalaidiuk <sasha.treyser90@gmail.com>
2024-09-19 12:09:59 +0300

    New feature: creating models

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import TaskType, Position, Task, Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+admin.site.register(TaskType)
+admin.site.register(Position)
+admin.site.register(Task)
--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position, on_delete=models.CASCADE, null=True, default=None
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} working as {self.position}"
+
+    class Meta:
+        verbose_name_plural = "Workers"
+        ordering = ["last_name", "first_name", "position", ]
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+
+    PRIORITY_CHOICES = [
+        ("Low", "Low"),
+        ("Medium", "Medium"),
+        ("High", "High"),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(choices=PRIORITY_CHOICES, max_length=6)
+    task_type = models.ForeignKey(
+        TaskType, on_delete=models.CASCADE, related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="tasks", blank=True
+    )
+
+    def __str__(self):
+        assignee_names = ", ".join(
+            str(assignee) for assignee in self.assignees.all()
+        )
+        return (f"{self.name} assigned to {assignee_names}"
+                f" with {self.priority} priority")
