ddafd8caba291c6c7d1dc7880a1dc3e71d19d0e7
achalaidiuk <sasha.treyser90@gmail.com>
2024-09-19 12:31:41 +0300

    New feature: creating index function

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Worker
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_workers = Worker.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(
+        request, "task_manager/index.html", context=context
+    )
