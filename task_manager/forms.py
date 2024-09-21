e18d01aa09b74245e31d75e7a8499baa6267142e
achalaidiuk <sasha.treyser90@gmail.com>
2024-09-21 18:18:02 +0300

    Refactor: fixed problems with new options

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,9 +1,15 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.forms import DateInput
 from .models import Task, Worker
 
 
 class TaskCreationForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True
+    )
 
     class Meta:
         model = Task
